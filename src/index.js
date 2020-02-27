756d7b6d7a8cefa68e31f8a7cd5f929a1503ab43
Isaac Avilez <isaac1180@aol.com>
2020-02-27 11:11:08 -0500

    we tried

--- src/index.js
+++ src/index.js
@@ -1,1 +1,76 @@
-const theatreId = null;
+const theatreId = "https://evening-plateau-54365.herokuapp.com/theatres/162"
+
+let theatreObjectsarr = [];
+
+const getTheatres =  () => {
+   return fetch(theatreId)
+   .then(res => res.json())
+   .then(theatre => {
+       theatreObjectsarr = theatre.showings
+       //As a user, when the page loads I should 
+       //see a list of movie showings fetched from a remote API.
+       theatre.showings.forEach(showingObject => {
+          const divCard = document.createElement('div')
+          divCard.setAttribute('class','card')
+          divCard.innerHTML = `
+          <div class="content">
+            <div class="header">
+                ${showingObject.film.title}
+         </div>
+         <div class="meta">
+         ${showingObject.film.runtime}
+         </div>
+         <div class="description" data-set="${showingObject.id}">
+             ${showingObject.tickets_sold} remaining tickets
+        </div>
+        <span class="ui label">
+          ${showingObject.showtime}
+        </span>
+    </div>
+          `
+          const divExtraContent = document.createElement('div')
+          divExtraContent.className = "extra-content"
+          divCard.append(divExtraContent)
+          const buttonDiv = document.createElement('div')
+          buttonDiv.className = "ui blue button"
+          buttonDiv.setAttribute('data-id', `${showingObject.id}`)
+          buttonDiv.innerText = "Buy Ticket"
+          divExtraContent.append(buttonDiv)
+        //   <div class="ui blue button" data-id="${showingObject.id}">Buy Ticket</div>
+
+          const showingDiv = document.querySelector('.showings')
+          showingDiv.append(divCard)
+          
+
+          buttonDiv.addEventListener('click', (e) => {
+              const getRemainingTickets = document.querySelector('.description')
+              fetch()
+              
+          })
+       });
+   })
+}
+
+
+getTheatres()
+
+
+/* <div class="card">
+  <div class="content">
+    <div class="header">
+      (Film Title)
+    </div>
+    <div class="meta">
+      (Runtime) minutes
+    </div>
+    <div class="description">
+      (Num Tickets) remaining tickets
+    </div>
+    <span class="ui label">
+      (Showtime)
+    </span>
+  </div>
+  <div class="extra content">
+    <div class="ui blue button">Buy Ticket</div>
+  </div>
+// </div> */
