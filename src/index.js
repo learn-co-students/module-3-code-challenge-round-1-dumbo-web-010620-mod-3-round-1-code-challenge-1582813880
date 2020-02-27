e271405464c2292acfc8e9bd6eebc043a3c786c3
Julianna Boyd <julianna.boyd91@gmail.com>
2020-02-27 12:25:42 -0500

    done

--- src/index.js
+++ src/index.js
@@ -5,30 +5,37 @@ document.addEventListener('DOMContentLoaded', () => {
     async function fetchMovies() {
         return fetch('https://evening-plateau-54365.herokuapp.com/theatres/158')
         .then(response => response.json())
-        .then(movies => movies.array.forEach(movie => {
-            renderMovies(movie)
+        .then(movies => movies.showings.forEach(movie => {
+            renderMovies(movie); 
         }))
     }
     
     function renderMovies(movieObj) {
         const allShowings = document.querySelector("div.ui.cards.showings")
         allShowings.innerHTML += `
-            <h4>${movieObj.showings[0]['film']}</h4>
-                <p>${movieObj.showings}</p>
-                    <p>${movieObj.showings}</p>`
-        
-        }
-
-
-
-
-
+                <div class="content">
+                <div class="header">
+                ${movieObj.film.title}
+                </div>
+                <div class="meta">
+                ${movieObj.film.runtime}
+                </div>
+                <div class="description">
+                ${movieObj.tickets_sold} remaining tickets
+                </div>
+                <span class="ui label">
+                ${movieObj.showtime}
+                </span>
+                </div>
+                <div class="extra content">
+                <div class="ui blue button">Buy Ticket</div>
+                </div>
+                </div>`
+    }
+       
+  
 fetchMovies()
     // end of DOMContentLoaded
 })
 
-
-//       <button class="like-btn">Like <3</button>
-//       <button class="delete-btn">Delete</button>
-//  </div>
-// `
+    
